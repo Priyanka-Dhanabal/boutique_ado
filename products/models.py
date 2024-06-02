c0d6a1a34cc558c84c473b2971cdd412b73d059d
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-06-02 12:16:17 +0000

    added products views

--- products/models.py
+++ products/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'categories'
+    
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250)
 
